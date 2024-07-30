c36dbb444971bd0d19c95a2d9e7a1a48d6c85625
AshokasAk <ashokkumarmoff@gmail.com>
2024-07-30 12:54:20 +0530

    foruth commit

--- main.js
+++ main.js
@@ -8,27 +8,7 @@ app.get ('/', (req,res) =>{
  res.json({ msg: "Hello students!" })
 })
 
-//CURd functionalities
 
-// R -for reading
-app.get('/movies',()=>{
-
-})
-
-// C - for creating
-app.post('/movies',()=>{
-
-})
-
-// U - for updating
-app.put('/movies/:id',()=>{
-
-})
-
-// D - for deleting
-app.delete('/movies/:id',()=>{
-
-})
 
 
 app.listen(PORT,()=>{
---
+++ router/movies.routes.js
@@ -0,0 +1,25 @@
+import exoress from 'express'
+
+const router = express.Router()
+
+//CURd functionalities
+
+// R -for reading
+router.get('/movies',()=>{
+
+})
+
+// C - for creating
+router.post('/movies',()=>{
+
+})
+
+// U - for updating
+router.put('/movies/:id',()=>{
+
+})
+
+// D - for deleting
+router.delete('/movies/:id',()=>{
+
+})
